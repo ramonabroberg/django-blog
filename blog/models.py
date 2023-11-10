9ab0b5f840ecb5abdb9fa597dc0d8845dd23497a
ramonabroberg <>
2023-11-10 14:46:47 +0000

    Add env.py file. Update models.py file.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = (("0", "Draft"), ("1", "Published"))
